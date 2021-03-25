b4a71054fb4a8c9d4cdd542622a2690b57eabd11
Somali-Hacker <68199580+Somali-Hacker@users.noreply.github.com>
2021-03-26 02:15:31 +0300

    Add files via upload

---
+++ moha.py
@@ -0,0 +1,32 @@
+import os , sys , time
+
+os.system('clear')
+os.system('git pull origin master')
+
+logo = """
+
+\033[0;31m ____    ____     ___     ____  ____        _       
+\033[0;31m|_   \  /   _|  .'   `.  |_   ||   _|      / \      
+\033[0;37m  |   \/   |   /  .-.  \   | |__| |       / _ \     
+\033[0;37m  | |\  /| |   | |   | |   |  __  |      / ___ \    
+\033[0;32m _| |_\/_| |_  \  `-'  /  _| |  | |_   _/ /   \ \_  
+\033[0;32m|_____||_____|  `.___.'  |____||____| |____| |____| 
+                                                    
+
+\033[1;97m----------------------------------------------------
+\033[1;93m➤\033[1;97m Author   : MoHa-Abdi
+\033[1;93m➤\033[1;97m Github   : https://github.com/Somali-Hacker
+\033[1;93m➤\033[1;97m Facebook : https://www.facebook.com/Moha.Abdi.2004
+\033[1;93m➤\033[1;97m WhatsApp : +252636722527
+\033[1;97m----------------------------------------------------
+"""
+
+print(logo)
+print("")
+print('\033[1;93m !! \033[1;97m Toolkan Waxaa saaray ownerka waayo waxa uu \033[0;31mKasoo hor jeeda \033[1;97mdiintena islaamka.')
+print('')
+print('\033[1;93m !! \033[1;97m Haddii aad \033[0;31mFacebookyo \033[1;97mold ah soo qabato kadibna iibiso, Fadlan ogow inay \033[0;31mXaaran \033[1;97mtahay lacagtaas xitaa haddii uu Facebook qof \033[0;31mGaal \033[1;97mah yahay.')
+print('')
+print('\033[1;93m !! \033[1;97m Wixii macluumad dheerad ah Halkan igalaso xidhiidh: +2520636722527, Ama  Facebook: https://www.facebook.com/Moha.Abdi.2004')
+print('')
+print('\033[0;31m NOTE\033[1;97m: This Tool is depreciated for ever!!')
